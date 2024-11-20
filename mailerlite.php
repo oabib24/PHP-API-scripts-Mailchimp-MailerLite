90529d48e02dc27ec24808a4e558ff2fd618a7e0
oabib24 <oabib24@gmail.com>
2024-11-20 01:25:06 -0500

    Update mailerlite.php

--- mailerlite.php
+++ mailerlite.php
@@ -1,11 +1,11 @@
+#!/usr/bin/php
 <?php
 
 error_reporting(0);
 ini_set('display_errors', 0);
 
-// Replace with your MailerLite API key
 $apiKey = '';
-$groupId = ''; // Group ID where the subscriber should be added
+$groupId = ''; 
 $apiUrl = '';
 
 $query_string = trim(fgets(STDIN));
@@ -17,19 +17,17 @@ $lname = isset($post['O-Last']) ? $post['O-Last'] : '';
 $optin = isset($post['O-EmailList']) ? $post['O-EmailList'] : null;
 
 if ($optin != null && $email != null) {
-    // Prepare subscriber data
     $subscriberData = array(
         'email' => $email,
         'fields' => array(
             'name' => $fname,
-            'last_name' => $lname,
+            'last_name' => $lname
         ),
-        'groups' => [$groupId] // Add subscriber to the group
+        'groups' => [$groupId] 
     );
 
     $data = json_encode($subscriberData);
 
-    // Initialize cURL
     $curl = curl_init();
 
     curl_setopt_array($curl, array(
@@ -45,7 +43,6 @@ if ($optin != null && $email != null) {
 
     $result = curl_exec($curl);
 
-    // Check for cURL errors
     if (curl_errno($curl)) {
         echo 'Request Error:' . curl_error($curl);
     } else {
